85736fed9faa5716af4755525e01e4a233324b27
hellboy <hellboy@gmail.com>
2016-05-07 10:44:05 +0800

    【+】项目被创建

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace XNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 31);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 389);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "序号";
+            this.Column1.Name = "Column1";
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 80F;
+            this.Column2.HeaderText = "记录";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 10F;
+            this.Column3.HeaderText = "分类";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 10F;
+            this.Column4.HeaderText = "来源";
+            this.Column4.Name = "Column4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "检索记录";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.textBox1.Location = new System.Drawing.Point(59, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(555, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(620, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "备份数据库";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 432);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XNote  Ver1.0.0";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SQLite;
+
+namespace XNote
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            var dt = Query("SELECT * FROM xnote");
+            //dataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            // 避免dgv加载时也触发changed事件
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+            // 禁止自动创建列
+            dataGridView1.AutoGenerateColumns = false;
+            // 绑定列数据源属性
+            dataGridView1.Columns[0].DataPropertyName = "序号";
+            dataGridView1.Columns[1].DataPropertyName = "记录";
+            dataGridView1.Columns[2].DataPropertyName = "分类";
+            dataGridView1.Columns[3].DataPropertyName = "来源";
+            // dgv绑定datatable
+            dataGridView1.DataSource = dt;
+            // 下拉到最后一行
+            dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[2];
+            // 设置背景色
+            var style = new DataGridViewCellStyle {BackColor = Color.FromArgb(91, 155, 213)};
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            {
+                col.HeaderCell.Style = style;
+            }
+            dataGridView1.EnableHeadersVisualStyles = false;
+
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(221, 235, 247);
+        }
+
+        
+        // 增+改
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            string sql;
+            var idx      = dataGridView1.Rows[e.RowIndex].Cells[0].Value + "";
+            var record   = dataGridView1.Rows[e.RowIndex].Cells[1].Value + "";
+            var catg     = dataGridView1.Rows[e.RowIndex].Cells[2].Value + "";
+            var from     = dataGridView1.Rows[e.RowIndex].Cells[3].Value + "";
+
+            if ("" == idx)
+            {
+                // 新增记录
+                sql = string.Format("INSERT INTO xnote(记录, 分类, 来源) VALUES('{0}','{1}','{2}')",
+                    record,
+                    catg,
+                    from);
+                Query(sql);
+
+                // 获取新增记录的行号
+                var dt = Query("SELECT MAX(序号) FROM xnote");
+                dataGridView1.Rows[e.RowIndex].Cells[0].Value = dt.Rows[0][0].ToString();
+            }
+            else
+            {
+               // 更新记录
+                sql = string.Format("UPDATE xnote SET 记录='{0}', 分类='{1}', 来源='{2}' where 序号='{3}'",
+                record,
+                catg,
+                from,
+                idx);
+                Query(sql);
+            }
+        }
+
+        // 删
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            var idx = e.Row.Cells[0].Value + "";
+            if (DialogResult.OK == MessageBox.Show("确认删除当前记录", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Question))
+            {
+                var sql = string.Format("DELETE FROM xnote WHERE 序号={0}", idx);
+                Query(sql);
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+
+        // 查
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var keywords = textBox1.Text.Split(' ');
+            var sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%'", keywords[0]);
+            if (2 == keywords.Length)
+            {
+                // 支持两个关键字搜索
+                sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%' AND 记录 LIKE '%{1}%'", keywords[0], keywords[1]);
+            }
+            var dt = Query(sql);
+            dataGridView1.DataSource = dt;
+        }
+
+        // 备份数据库
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dt       = DateTime.Now;
+            var filename = dt.ToString("yyyy-MM-dd")+".db";
+            File.Copy("hangch.db", filename, true);
+            if (File.Exists(filename))
+            {
+                MessageBox.Show("备份成功!");
+            }
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var rownum = e.RowIndex;
+            var rct = new Rectangle(e.RowBounds.Location.X, e.RowBounds.Location.Y + 4, ((DataGridView)sender).RowHeadersWidth - 4, e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics, rownum.ToString(), ((DataGridView)sender).RowHeadersDefaultCellStyle.Font, rct, ((DataGridView)sender).RowHeadersDefaultCellStyle.ForeColor, Color.Transparent, TextFormatFlags.Right);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+            {
+                textBox1.Clear();
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL语句，返回datatable
+        /// </summary>
+        /// <param name="sql">SQL语句</param>
+        /// <returns></returns>
+        static private DataTable Query(string sql)
+        {
+
+            using (var conn = new SQLiteConnection("Data Source=hangch.db"))
+            {
+                conn.Open();
+                var cmd = new SQLiteDataAdapter(sql, conn);
+                var dt = new DataTable();
+                cmd.Fill(dt);
+
+                return dt;
+            }
+        }
+
+        // rowhead显示行号
+
+        
+    }
+
+     
+
+
+}

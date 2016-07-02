34f850d6116a9853fe198bad5e67d150c90fe089
hellboy <hellboy@gmail.com>
2016-07-02 12:42:19 +0800

    【+】增加匹配标准文本功能；打包db和dll文件进程序集
    【*】简化新增记录操作
    【-】删除Ctrl+F快捷键

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,43 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgv = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuOpenStandrad = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSetDirectory = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuBackUP = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuViewPicture = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNewItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgv.AllowUserToOrderColumns = true;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeight = 27;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeight = 27;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3,
             this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(0, 34);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.DarkBlue;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(814, 421);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
-            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            this.dgv.Location = new System.Drawing.Point(0, 34);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.dgv.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgv.RowTemplate.Height = 23;
+            this.dgv.Size = new System.Drawing.Size(814, 421);
+            this.dgv.TabIndex = 0;
+            this.dgv.TabStop = false;
+            this.dgv.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv_CellMouseDown);
+            this.dgv.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgv_RowPostPaint);
+            this.dgv.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dgv_UserDeletingRow);
             // 
             // Column1
             // 
@@ -80,8 +91,8 @@
             // 
             // Column3
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.Column3.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
             this.Column3.FillWeight = 10F;
             this.Column3.HeaderText = "  分类";
             this.Column3.Name = "Column3";
@@ -101,47 +112,92 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "检索记录";
             // 
-            // textBox1
+            // txt
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.textBox1.Location = new System.Drawing.Point(69, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(633, 21);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txt.Location = new System.Drawing.Point(66, 5);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(745, 21);
+            this.txt.TabIndex = 0;
+            this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            this.txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
             // 
-            // button1
+            // contextMenuStrip1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(710, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 25);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "备份数据库";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNewItem,
+            this.toolStripSeparator2,
+            this.mnuOpenStandrad,
+            this.mnuSetDirectory,
+            this.mnuViewPicture,
+            this.toolStripSeparator1,
+            this.mnuBackUP});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 148);
+            // 
+            // mnuOpenStandrad
+            // 
+            this.mnuOpenStandrad.Name = "mnuOpenStandrad";
+            this.mnuOpenStandrad.Size = new System.Drawing.Size(136, 22);
+            this.mnuOpenStandrad.Text = "打开标准";
+            this.mnuOpenStandrad.Click += new System.EventHandler(this.mnuOpenStandrad_Click);
+            // 
+            // mnuSetDirectory
+            // 
+            this.mnuSetDirectory.Name = "mnuSetDirectory";
+            this.mnuSetDirectory.Size = new System.Drawing.Size(136, 22);
+            this.mnuSetDirectory.Text = "设置目录";
+            this.mnuSetDirectory.Click += new System.EventHandler(this.mnuSetDirectory_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(133, 6);
+            // 
+            // mnuBackUP
+            // 
+            this.mnuBackUP.Name = "mnuBackUP";
+            this.mnuBackUP.Size = new System.Drawing.Size(136, 22);
+            this.mnuBackUP.Text = "备份数据库";
+            this.mnuBackUP.Click += new System.EventHandler(this.mnuBackUP_Click);
+            // 
+            // mnuViewPicture
+            // 
+            this.mnuViewPicture.Name = "mnuViewPicture";
+            this.mnuViewPicture.Size = new System.Drawing.Size(136, 22);
+            this.mnuViewPicture.Text = "查看图片";
+            this.mnuViewPicture.Click += new System.EventHandler(this.mnuViewPicture_Click);
+            // 
+            // mnuNewItem
+            // 
+            this.mnuNewItem.Name = "mnuNewItem";
+            this.mnuNewItem.Size = new System.Drawing.Size(152, 22);
+            this.mnuNewItem.Text = "新增记录";
+            this.mnuNewItem.Click += new System.EventHandler(this.mnuNewItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(133, 6);
             // 
             // Form1
             // 
-            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 468);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgv);
             this.Font = new System.Drawing.Font("宋体", 10F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "XNote  Ver1.0.0";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,14 +205,21 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpenStandrad;
+        private System.Windows.Forms.ToolStripMenuItem mnuSetDirectory;
+        private System.Windows.Forms.ToolStripMenuItem mnuBackUP;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuViewPicture;
+        private System.Windows.Forms.ToolStripMenuItem mnuNewItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
-using System.Drawing;
-using System.Windows.Forms;
-using System.IO;
 using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace XNote
 {
@@ -11,44 +15,53 @@ namespace XNote
     {
         public Form1()
         {
-            
             InitializeComponent();
 
-            // 避免dgv加载时也触发changed事件
-            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+            // 设置标题栏
+            // 版本号组成：主版本+次版本+内部版本号+修订号
+            var title = string.Format("XNote Ver{0}   『C0der by hangch』", Application.ProductVersion);
+            Text = title;
+
+            // 释放资源
+            ReleaseResource("System.Data.SQLite.dll", "XNote.System.Data.SQLite.dll");
+            ReleaseResource("XNote.db", "XNote.XNote.db");
+
+            // 在控件初始化(InitializeComponent)后才订阅CellValueChanged事件
+            dgv.CellValueChanged += dgv_CellValueChanged;
+
             // 下拉到最后一行
-            dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[2];
+            dgv.CurrentCell = dgv.Rows[dgv.Rows.Count - 1].Cells[2];
+
             // 设置背景色
-            var style = new DataGridViewCellStyle { BackColor = Color.FromArgb(237, 125, 49) };
-            foreach (DataGridViewColumn col in dataGridView1.Columns)
+            var style = new DataGridViewCellStyle {BackColor = Color.FromArgb(237, 125, 49)};
+            foreach (DataGridViewColumn col in dgv.Columns)
             {
                 col.HeaderCell.Style = style;
             }
-            dataGridView1.EnableHeadersVisualStyles = false;
-            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(252, 228, 214);
+            dgv.EnableHeadersVisualStyles = false;
+            dgv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(252, 228, 214);
 
-            
             // 禁止自动创建列
-            dataGridView1.AutoGenerateColumns = false;
+            dgv.AutoGenerateColumns = false;
             // 绑定列数据源属性
-            dataGridView1.Columns[0].DataPropertyName = "序号";
-            dataGridView1.Columns[1].DataPropertyName = "记录";
-            dataGridView1.Columns[2].DataPropertyName = "分类";
-            dataGridView1.Columns[3].DataPropertyName = "来源";
+            dgv.Columns[0].DataPropertyName = "序号";
+            dgv.Columns[1].DataPropertyName = "记录";
+            dgv.Columns[2].DataPropertyName = "分类";
+            dgv.Columns[3].DataPropertyName = "来源";
             var dt = Query("SELECT * FROM xnote");
+
             // dgv绑定datatable
-            dataGridView1.DataSource = dt;
+            dgv.DataSource = dt;
         }
 
-        
         // 增+改
-        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        private void dgv_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
             string sql;
-            var idx      = dataGridView1.Rows[e.RowIndex].Cells[0].Value + "";
-            var record   = dataGridView1.Rows[e.RowIndex].Cells[1].Value + "";
-            var catg     = dataGridView1.Rows[e.RowIndex].Cells[2].Value + "";
-            var from     = dataGridView1.Rows[e.RowIndex].Cells[3].Value + "";
+            var idx = dgv.Rows[e.RowIndex].Cells[0].Value + "";
+            var record = dgv.Rows[e.RowIndex].Cells[1].Value + "";
+            var catg = dgv.Rows[e.RowIndex].Cells[2].Value + "";
+            var from = dgv.Rows[e.RowIndex].Cells[3].Value + "";
 
             if ("" == idx)
             {
@@ -61,25 +74,26 @@ namespace XNote
 
                 // 获取新增记录的行号
                 var dt = Query("SELECT MAX(序号) FROM xnote");
-                dataGridView1.Rows[e.RowIndex].Cells[0].Value = dt.Rows[0][0].ToString();
+                dgv.Rows[e.RowIndex].Cells[0].Value = dt.Rows[0][0] + "";
             }
             else
             {
-               // 更新记录
+                // 更新记录
                 sql = string.Format("UPDATE xnote SET 记录='{0}', 分类='{1}', 来源='{2}' where 序号='{3}'",
-                record,
-                catg,
-                from,
-                idx);
+                    record,
+                    catg,
+                    from,
+                    idx);
                 Query(sql);
             }
         }
 
         // 删
-        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        private void dgv_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
         {
-            var idx = e.Row.Cells[0].Value + "";
-            if (DialogResult.OK == MessageBox.Show("确认删除当前记录", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Question))
+            var idx = dgv.SelectedRows[0].Cells[0].Value + "";
+            if (DialogResult.OK ==
+                MessageBox.Show("确认删除当前记录", "警告", MessageBoxButtons.OKCancel, MessageBoxIcon.Question))
             {
                 var sql = string.Format("DELETE FROM xnote WHERE 序号={0}", idx);
                 Query(sql);
@@ -90,61 +104,112 @@ namespace XNote
             }
         }
 
-
-        // 查
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void dgv_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
         {
-            var keywords = textBox1.Text.Split(' ');
-            var sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%'", keywords[0]);
-            if (2 == keywords.Length)
+            if (e.Button == MouseButtons.Right)
             {
-                // 支持两个关键字搜索
-                sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%' AND 记录 LIKE '%{1}%'", keywords[0], keywords[1]);
-            }
-            var dt = Query(sql);
-            dataGridView1.DataSource = dt;
-        }
-
-        // 备份数据库
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var dt       = DateTime.Now;
-            var filename = dt.ToString("yyyy-MM-dd")+".db";
-            File.Copy("hangch.db", filename, true);
-            if (File.Exists(filename))
-            {
-                MessageBox.Show("备份成功!");
-            }
-        }
-
-        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
-        {
-            var rownum = e.RowIndex;
-            var rct = new Rectangle(e.RowBounds.Location.X, e.RowBounds.Location.Y + 4, ((DataGridView)sender).RowHeadersWidth - 4, e.RowBounds.Height);
-            TextRenderer.DrawText(e.Graphics, rownum.ToString(), ((DataGridView)sender).RowHeadersDefaultCellStyle.Font, rct, ((DataGridView)sender).RowHeadersDefaultCellStyle.ForeColor, Color.Transparent, TextFormatFlags.Right);
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape && textBox1.Focused)
-            {
-                textBox1.Clear();
-            }
-            else if(e.Modifiers == Keys.Control && e.KeyCode == Keys.F)
-            {
-                textBox1.Focus();
+                contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+                dgv.ClearSelection();
+                dgv.Rows[e.RowIndex].Selected = true;
             }
         }
 
         /// <summary>
-        /// 执行SQL语句，返回datatable
+        /// 显示行号
+        /// </summary>
+        private void dgv_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var rownum = e.RowIndex;
+            var rct = new Rectangle(e.RowBounds.Location.X, e.RowBounds.Location.Y + 4,
+                ((DataGridView)sender).RowHeadersWidth - 4, e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics, rownum.ToString(), ((DataGridView)sender).RowHeadersDefaultCellStyle.Font,
+                rct, ((DataGridView)sender).RowHeadersDefaultCellStyle.ForeColor, Color.Transparent,
+                TextFormatFlags.Right);
+        }
+
+        // 查
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            var keywords = txt.Text.Split(' ');
+            var sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%'", keywords[0]);
+            if (2 == keywords.Length)
+            {
+                // 支持两个关键字搜索
+                sql = string.Format("SELECT * FROM xnote WHERE 记录 LIKE '%{0}%' AND 记录 LIKE '%{1}%'", keywords[0],
+                    keywords[1]);
+            }
+            var dt = Query(sql);
+            dgv.DataSource = dt;
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (27 == e.KeyChar)
+            {
+                txt.Clear();
+            }
+        }
+
+        private void mnuNewItem_Click(object sender, EventArgs e)
+        {
+            dgv.FirstDisplayedCell = dgv.Rows[dgv.Rows.Count - 1].Cells[1]; 
+            dgv.CurrentCell = dgv.Rows[dgv.Rows.Count - 1].Cells[1];
+            dgv.BeginEdit(true);
+        }
+
+        private void mnuOpenStandrad_Click(object sender, EventArgs e)
+        {
+            const string sql = "SELECT 标准目录 FROM config";
+            var standardDirectory = Query(sql).Rows[0][0] + "";
+            if (!Directory.Exists(standardDirectory))
+            {
+                MessageBox.Show("请设置标准规范所在目录", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            var standards = new Dictionary<string, string>();
+            FindFiles(standardDirectory, standards);
+            var standard = dgv.SelectedRows[0].Cells[3].Value + "";
+            standard = standard.Substring(0, standard.LastIndexOf(' '));
+            foreach (var item in standards.Where(item => item.Key.Contains(standard)))
+            {
+                Process.Start(item.Value);
+            }
+        }
+
+        private void mnuSetDirectory_Click(object sender, EventArgs e)
+        {
+            var standardDirectory = SelectPath("请选择标准文件目录");
+            if (Directory.Exists(standardDirectory))
+            {
+                var sql = string.Format("UPDATE config SET 标准目录='{0}'", standardDirectory);
+                Query(sql);
+            }
+        }
+
+        private void mnuViewPicture_Click(object sender, EventArgs e)
+        {
+            //TODO
+        }
+
+        private void mnuBackUP_Click(object sender, EventArgs e)
+        {
+            var dt = DateTime.Now;
+            var filename = dt.ToString("yyyy-MM-dd") + ".db";
+            File.Copy("XNote.db", filename, true);
+            if (File.Exists(filename))
+            {
+                MessageBox.Show("备份成功");
+            }
+        }
+
+        /// <summary>
+        ///     执行SQL语句，返回datatable
         /// </summary>
         /// <param name="sql">SQL语句</param>
         /// <returns></returns>
-        static private DataTable Query(string sql)
+        private static DataTable Query(string sql)
         {
-
-            using (var conn = new SQLiteConnection("Data Source=hangch.db"))
+            using (var conn = new SQLiteConnection("Data Source=XNote.db"))
             {
                 conn.Open();
                 var cmd = new SQLiteDataAdapter(sql, conn);
@@ -155,12 +220,68 @@ namespace XNote
             }
         }
 
-        
+        /// <summary>
+        ///  选择文件夹
+        /// </summary>
+        /// <param name="description">选择文件夹对话框说明</param>
+        /// <returns></returns>
+        public static string SelectPath(string description)
+        {
+            var fbd = new FolderBrowserDialog {Description = description};
+            fbd.ShowDialog();
+            return fbd.SelectedPath;
+        }
 
-        
+        /// <summary>
+        /// 释放资源（资源属性映射为嵌入式的资源）
+        /// </summary>
+        /// <param name="filePath">释放到的路径</param>
+        /// <param name="resourceName">资源全名“命名空间.+资源名”</param>
+        public static void ReleaseResource(string filePath, string resourceName)
+        {
+            // 针对所有流文件
+            // 获取当前正在执行的程序集
+            var assembly = Assembly.GetExecutingAssembly();
+            //foreach (var file in assembly.GetManifestResourceNames())
+            // // 辅助方法：获取资源在程序集内部的名称
+            // MessageBox.Show(file);
+            var stream = assembly.GetManifestResourceStream(resourceName);
+            // 将流读取到二进制数组中
+            if (stream != null)
+            {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                // 将二进制写入文件
+                if (File.Exists(filePath)) return;
+                var fs = new FileStream(filePath, FileMode.CreateNew, FileAccess.Write);
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// 遍历目录及其子目录下的所有文件
+        /// </summary>
+        /// <param name="baseDirectory">起始目录</param>
+        /// <param name="files">存放返回值的字典（文件名-文件路径对）</param>
+        public static void FindFiles(string baseDirectory, Dictionary<string, string> files)
+        {
+            //在指定目录及子目录下查找文件
+            var directoryInfo = new DirectoryInfo(baseDirectory);
+
+            // 查找子目录
+            foreach (var d in directoryInfo.GetDirectories())
+            {
+                FindFiles(directoryInfo + "\\" + d + "\\", files);
+            }
+
+            // 查找文件
+            foreach (var f in directoryInfo.GetFiles().Where(f => !files.Keys.Contains(f.Name)))
+            {
+                files.Add(f.Name, f.FullName);
+            }
+        }
     }
-
-     
-
 
 }

31c95c6110c362c6ba8b5d702abcd6fa06f1ec97
LIM42SNEC <lim42snec@gmail.com>
2018-07-21 22:26:37 +0800

    [!]:hammer:开启WPF新纪元

---
+++ Model/Record.cs
@@ -0,0 +1,13 @@
+﻿using PropertyChanged;
+
+namespace XNote.Model
+{
+    [ImplementPropertyChanged]
+    class Record
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Reference { get; set; }
+       
+    }
+}
